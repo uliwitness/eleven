be685339c98af95038dafef3e043c4f5a8275277
Uli Kusterer <ulivc@zathras.de>
2014-11-01 00:32:16 +0100

    Initial Commit

---
+++ eleven_chatserver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eleven_chatserver
+//
+//  Created by Uli Kusterer on 2014-11-01.
+//  Copyright (c) 2014 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
