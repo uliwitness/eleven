0340f4f31ab2dd56572bdc5f6d87c3ba57c9fa78
Uli Kusterer <ulivc@zathras.de>
2014-11-01 02:56:54 +0100

    Don't parrot back messages we were sent by default, only in the sample.

--- eleven_chatserver/main.cpp
+++ eleven_chatserver/main.cpp
@@ -174,7 +174,7 @@ public:
                 }
                 else
                 {
-                	session.printf("JUNK:%s\n",commandName.c_str());
+                	session.printf("\n");	// So the other side doesn't wait for an answer forever.
                 }
             }
             close( sessionSocket );
@@ -204,12 +204,17 @@ int main()
         printf( "Listening on port %d\n", server.port_number() );
 		server.register_command_handler( "/bye", []( eleven_session* session, std::string currRequest )
 		{
-			session->printf("Nice to speak with you.\n");
+			session->printf( "Nice to speak with you.\n" );
 			return false;	// Terminate session!
 		} );
 		server.register_command_handler( "/howdy", []( eleven_session* session, std::string currRequest )
 		{
-			session->printf("Welcome!\n");
+			session->printf( "Welcome!\n" );
+			return true;
+		} );
+		server.register_command_handler( "*", []( eleven_session* session, std::string currRequest )
+		{
+			session->printf( "JUNK:%s\n",currRequest.c_str() );
 			return true;
 		} );
         server.wait_for_connection();
